6d5e682f6a5a42b080555405b9bc4c7edb628c67
mdontsov <mdontsov@live.com>
2018-06-23 12:41:14 +0300

    ...

--- frontend/src/components/Edit.js
+++ frontend/src/components/Edit.js
@@ -4,8 +4,8 @@ import {Link} from 'react-router-dom';
 
 class Edit extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
             user: {}
         };
